ffb1b06df1022cf3f9e1c44592310b5de8c15fac
Jesus Velez <jvf_velez@hotmail.com>
2020-11-11 21:17:11 -0600

    Se agrega parte de la logica para mostrar los clientes en espera y los clientes atendidos

--- js/app.js
+++ js/app.js
@@ -3,6 +3,13 @@ let arrayClientesRegistrados = [];
 let arrayClientesEnEspera = [];
 let arrayClientesAtendidos = [];
 
+// Datos precargados para ejemplo
+arrayClientesAtendidos.push(new Cliente('MariPer25', 'Maria', 'Peralta', 'Mujer', 25, 'VIP'));
+arrayClientesAtendidos.push(new Cliente('KarlHer29', 'Karla', 'Hernandez', 'Mujer', 29, 'Premiun'));
+arrayClientesAtendidos.push(new Cliente('JoseAnc27', 'Jose', 'Ancona', 'Hombre', 27, 'Basico'));
+arrayClientesEnEspera.push(new Cliente('MariPer25', 'Maria', 'Peralta', 'Mujer', 25, 'VIP'));
+arrayClientesEnEspera.push(new Cliente('PepeMor28', 'Pepe', 'Morelos', 'Hombre', 28, 'Premiun'));
+
 // Estructura que debe tener la informacion de nuestros clientes
 function Cliente(identificacion, nombre, apellido, genero, edad, tipoUsuario){
     this.identificacion =  identificacion;
@@ -30,14 +37,37 @@ let generoRegistrar = document.getElementById('genero_registrar');
 let edadRegistrar = document.getElementById('edad_registrar');
 let tipoRegistrar = document.getElementById('tipo_registrar');
 let menuSolicitarTurno = document.getElementById('main_seccion--solicitar--turno');
+let menuListarClientesEnEspera = document.getElementById('main_seccion_listar--clientes--espera');
+let listaClientesEnEspera = document.getElementById('lista_clientes_espera');
+let menuListarClientesAtendidos = document.getElementById('main_seccion--listar--clientes--atendidos');
+let listaClientesAtendidos = document.getElementById('lista_clientes_atendidos');
 
 // Funciones Utiles
-function desplegarMenus(menu){
+function desplegarMenus(menu, elementos){
     let status = menu.hidden;
+    if(elementos){
+        limpiarListaClientes(elementos);
+    }
     if(status == true){
         menu.hidden = false;
     }else{
         menu.hidden = true;
+    }
+}
+
+function listarClientes(elementoLista, arrayClientes){
+    for(let i = 0; i < arrayClientes.length; i++){
+        let item = document.createElement('li');
+        elementoLista.appendChild(item);
+        item.innerHTML = arrayClientes[i].nombre;
+    }
+}
+
+function limpiarListaClientes(elementoLista){
+    if(elementoLista.hasChildNodes()){
+        while(elementoLista.childNodes.length >= 1){
+            elementoLista.removeChild(elementoLista.firstChild);
+        }
     }
 }
 
@@ -56,14 +86,16 @@ buttonRegistrarEnviar.addEventListener('click', () => {
 });
 
 buttonSolicitarTurno.addEventListener('click', () => {
-
+    desplegarMenus(menuSolicitarTurno);
 });
 buttonAtenderCliente.addEventListener('click', () => {
 
 });
 buttonListarClientesEnEspera.addEventListener('click', () => {
-
+    desplegarMenus(menuListarClientesEnEspera, listaClientesEnEspera);
+    listarClientes(listaClientesEnEspera, arrayClientesEnEspera);
 });
 buttonListarClientesAtendidos.addEventListener('click', () => {
-
+    desplegarMenus(menuListarClientesAtendidos, listaClientesAtendidos);
+    listarClientes(listaClientesAtendidos, arrayClientesAtendidos);
 });
