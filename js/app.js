e09bd53cdcaac710c8d7ffc854c4062791916abe
Jesus Velez <jvf_velez@hotmail.com>
2020-11-13 20:51:17 -0600

    Se avanza en la logica para solicitar turno, falta agregar la funcion para ordenar la lista de espera de clientes

--- js/app.js
+++ js/app.js
@@ -4,22 +4,23 @@ let arrayClientesEnEspera = [];
 let arrayClientesAtendidos = [];
 
 // Datos precargados para ejemplo
-arrayClientesRegistrados.push(new Cliente('JesuGut22', 'Jesus', 'Gutierrez', 'Hombre', 22, 'VIP'));
-arrayClientesRegistrados.push(new Cliente('PaolFlo30', 'Paola', 'Flores', 'Mujer', 30, 'Basico'));
-arrayClientesAtendidos.push(new Cliente('MariPer25', 'Maria', 'Peralta', 'Mujer', 25, 'VIP'));
-arrayClientesAtendidos.push(new Cliente('KarlHer29', 'Karla', 'Hernandez', 'Mujer', 29, 'Premiun'));
-arrayClientesAtendidos.push(new Cliente('JoseAnc27', 'Jose', 'Ancona', 'Hombre', 27, 'Basico'));
-arrayClientesEnEspera.push(new Cliente('MariPer25', 'Maria', 'Peralta', 'Mujer', 25, 'VIP'));
-arrayClientesEnEspera.push(new Cliente('PepeMor28', 'Pepe', 'Morelos', 'Hombre', 28, 'Premiun'));
+arrayClientesRegistrados.push(new Cliente('JesuGut22', 'Jesus', 'Gutierrez', 'Hombre', 22, 'VIP', '-'));
+arrayClientesRegistrados.push(new Cliente('PaolFlo30', 'Paola', 'Flores', 'Mujer', 30, 'Basico', '-'));
+arrayClientesAtendidos.push(new Cliente('MariPer25', 'Maria', 'Peralta', 'Mujer', 25, 'VIP', 'VI0'));
+arrayClientesAtendidos.push(new Cliente('KarlHer29', 'Karla', 'Hernandez', 'Mujer', 29, 'Premiun', 'PR0'));
+arrayClientesAtendidos.push(new Cliente('JoseAnc27', 'Jose', 'Ancona', 'Hombre', 27, 'Basico', 'BA0'));
+arrayClientesEnEspera.push(new Cliente('MariPer25', 'Maria', 'Peralta', 'Mujer', 25, 'VIP', 'VI1'));
+arrayClientesEnEspera.push(new Cliente('PepeMor28', 'Pepe', 'Morelos', 'Hombre', 28, 'Premiun', 'PR1'));
 
 // Estructura que debe tener la informacion de nuestros clientes
-function Cliente(identificacion, nombre, apellido, genero, edad, tipoUsuario){
+function Cliente(identificacion, nombre, apellido, genero, edad, tipoUsuario, turno){
     this.identificacion =  identificacion;
     this.nombre = nombre;
     this.apellido = apellido;
     this.genero = genero;
     this.edad = edad;
     this.tipoUsuario = tipoUsuario;
+    this.turno = turno;
 }
 
 // Botones de las acciones principales(Registrar, Solicitar Turno, Atender Cliente, Listar Clientes En Espera, Listar Clientes Atendidos)
@@ -40,6 +41,8 @@ let generoRegistrar = document.getElementById('genero_registrar');
 let edadRegistrar = document.getElementById('edad_registrar');
 let tipoRegistrar = document.getElementById('tipo_registrar');
 let menuSolicitarTurno = document.getElementById('main_seccion--solicitar--turno');
+let noIdentificacionSolicitarTurno = document.getElementById('id_solicitar--turno--enviar');
+let tipoCliente = document.getElementById('tipo_cliente');
 let mostrarClienteAtendido = document.getElementById('cliente_atendido');
 let menuListarClientesEnEspera = document.getElementById('main_seccion_listar--clientes--espera');
 let listaClientesEnEspera = document.getElementById('lista_clientes_espera');
@@ -67,25 +70,56 @@ function verificarCamposVacios(){
     }
 }
 
-function verificarClientesExistentes(arrayClientes){
+function verificarClientesExistentes(arrayClientes, elemento){
     for(let i = 0; i < arrayClientes.length; i++){
-        if(arrayClientes[i].identificacion == idRegistrar.value){
+        if(arrayClientes[i].identificacion == elemento.value){
             return true;
-        }else{
-            return false;
         }
     }
+}
+
+function comprobarCliente(arrayClientes, elemento){
+    for(let i = 0; i < arrayClientes.length; i++){
+        if(arrayClientes[i].identificacion == elemento.value){
+            return arrayClientes[i];
+        }
+    }
+}
+
+// Variables Globales
+let basico = 2;
+let premiun = 2;
+let vip = 2;
+let noRegistrado = 0;
+
+function generarTurno(tipoUsuario){
+    switch (tipoUsuario) {
+        case 'Basico':
+            return `BA${basico++}`;
+        case 'Premiun':
+            return `PR${premiun++}`;
+        case 'VIP':
+            return `VI${vip++}`;
+        case 'Cliente No Registrado':
+            return `NR${noRegistrado++}`;
+        default:
+            break;
+    }
+}
+
+function ordenarClientesEnEspera(idCliente){
+
 }
 
 function listarClientes(elementoLista, arrayClientes){
     let encabezados = document.createElement('tr');
     elementoLista.appendChild(encabezados);
-    encabezados.innerHTML = `<th>No.</th> <th>ID</th> <th>Nombre Completo</th> <th>Genero</th> <th>Edad</th> <th>Tipo de Usuario</th>`;
+    encabezados.innerHTML = `<th>Turno</th> <th>ID</th> <th>Nombre Completo</th> <th>Genero</th> <th>Edad</th> <th>Tipo de Usuario</th>`;
     
     for(let i = 0; i < arrayClientes.length; i++){
         let item = document.createElement('tr');
         elementoLista.appendChild(item);
-        item.innerHTML = ` <td> ${i + 1} </td> <td> ${arrayClientes[i].identificacion} </td> <td> ${arrayClientes[i].nombre} ${arrayClientes[i].apellido} </td> <td> ${arrayClientes[i].genero} </td> <td> ${arrayClientes[i].edad} </td> <td> ${arrayClientes[i].tipoUsuario} </td>`;
+        item.innerHTML = ` <td> ${arrayClientes[i].turno} </td> <td> ${arrayClientes[i].identificacion} </td> <td> ${arrayClientes[i].nombre} ${arrayClientes[i].apellido} </td> <td> ${arrayClientes[i].genero} </td> <td> ${arrayClientes[i].edad} </td> <td> ${arrayClientes[i].tipoUsuario} </td>`;
     }
 }
 
@@ -104,7 +138,7 @@ buttonRegistrar.addEventListener('click', () => {
 
 buttonRegistrarEnviar.addEventListener('click', () => {
     let camposVacios = verificarCamposVacios();
-    let clienteExistente = verificarClientesExistentes(arrayClientesRegistrados);
+    let clienteExistente = verificarClientesExistentes(arrayClientesRegistrados, idRegistrar.value);
     if(camposVacios == true){
         alert('Por favor rellena todos los campos');
     }else{
@@ -121,8 +155,35 @@ buttonSolicitarTurno.addEventListener('click', () => {
 });
 
 buttonSolicitarTurnoEnviar.addEventListener('click', () => {
-    
-})
+    if(noIdentificacionSolicitarTurno.value == ''){
+        alert('Por favor escribe tu No. Identificacion');
+    }else{
+        let clienteExistenteEnEspera = verificarClientesExistentes(arrayClientesEnEspera, noIdentificacionSolicitarTurno);
+        if(clienteExistenteEnEspera == true){
+            let turnoExistente = comprobarCliente(arrayClientesEnEspera, noIdentificacionSolicitarTurno);
+            alert(`El ID: ${turnoExistente.identificacion} ya tiene el turno: ${turnoExistente.turno}`);
+        }else{
+            let clienteExistente = verificarClientesExistentes(arrayClientesRegistrados, noIdentificacionSolicitarTurno);
+            if(clienteExistente == true && tipoCliente.value == 'Registrado'){
+                let cliente = comprobarCliente(arrayClientesRegistrados, noIdentificacionSolicitarTurno);
+                let turno = generarTurno(cliente.tipoUsuario);
+                cliente.turno = turno;
+                arrayClientesEnEspera.push(cliente);
+            }
+            if(clienteExistente == null && tipoCliente.value == 'Nuevo'){
+                let clienteNoRegistrado = 'Cliente No Registrado';
+                let clienteNoRegistradoTurno = generarTurno(clienteNoRegistrado);
+                arrayClientesEnEspera.push(new Cliente(noIdentificacionSolicitarTurno.value, '-', '-', '-', 0, clienteNoRegistrado, clienteNoRegistradoTurno));
+            }
+            if(clienteExistente == true && tipoCliente.value == 'Nuevo'){
+                alert(`${noIdentificacionSolicitarTurno.value} ya esta registrado, cambie el tipo de cliente para continuar`);
+            }
+            if(clienteExistente == null && tipoCliente.value == 'Registrado'){
+                alert(`${noIdentificacionSolicitarTurno.value} no esta registrado, cambie el tipo de cliente para continuar`);
+            }
+        }
+    }
+});
 
 buttonAtenderCliente.addEventListener('click', () => {
     if(arrayClientesEnEspera.length == 0){
@@ -131,6 +192,10 @@ buttonAtenderCliente.addEventListener('click', () => {
         let clienteAtendido = arrayClientesEnEspera.shift();
         mostrarClienteAtendido.innerHTML = `ID: ${clienteAtendido.identificacion} - Nombre Completo: ${clienteAtendido.nombre} ${clienteAtendido.apellido} - Genero: ${clienteAtendido.genero} - Edad: ${clienteAtendido.edad} - Tipo de Usuario: ${clienteAtendido.tipoUsuario}`;
         arrayClientesAtendidos.push(clienteAtendido);
+        limpiarListaClientes(listaClientesEnEspera);
+        listarClientes(listaClientesEnEspera, arrayClientesEnEspera);
+        limpiarListaClientes(listaClientesAtendidos);
+        listarClientes(listaClientesAtendidos, arrayClientesAtendidos);
     }
 });
 
